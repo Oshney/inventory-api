552dd050037c10640c952633f2319fe8c3274088
Oshney Thakur <152894305+Oshney@users.noreply.github.com>
2026-03-26 17:01:24 +0530

    Update stock.js

--- api/stock.js
+++ api/stock.js
@@ -1,20 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.URL,
-  process.env.KEY
-);
-
 export default async function handler(req, res) {
+
+  const supabase = createClient(
+    process.env.URL,
+    process.env.KEY
+  );
 
   const { data, error } = await supabase
     .from("stock")
     .select("*");
 
   if (error) {
-    res.status(500).json(error);
-    return;
+    return res.status(500).json({ error });
   }
 
-  res.status(200).json(data);
+  return res.status(200).json(data);
 }
